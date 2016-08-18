fa903d653d27b64276ac753af2babbb2a6770d58
Morton Jonuschat <m.jonuschat@mojocode.de>
2016-08-18 17:25:15 +0200

    [TASK] Add assertions to ReferenceDefinitionTest

--- Tests/Unit/ReferenceDefinitionTest.php
+++ Tests/Unit/ReferenceDefinitionTest.php
@@ -16,77 +16,153 @@ namespace MojoCode\SqlParser\Tests\Unit;
  * The TYPO3 project - inspiring people to share!
  */
 
+use MojoCode\SqlParser\AST\ReferenceDefinition;
 use MojoCode\SqlParser\Parser;
 use PHPUnit\Framework\TestCase;
 
 class ReferenceDefinitionTest extends TestCase
 {
+    /**
+     * Each parameter array consists of the following values:
+     *  - reference definition SQL fragment
+     *  - expected table Name
+     *  - array of index column definitions [name, length, direction]
+     *  - MATCH value
+     *  - ON DELETE value
+     *  - ON UPDATE value
+     *
+     * @return array
+     */
     public function canParseReferenceDefinitionDataProvider(): array
     {
         return [
             'REFERENCES `anotherTable`(`aColumn`)' => [
                 'REFERENCES `anotherTable`(`aColumn`)',
-                null
+                'anotherTable',
+                [['aColumn', 0, null]],
+                null,
+                null,
+                null,
             ],
             'REFERENCES `anotherTable`(`aColumn`, anotherColumn)' => [
                 'REFERENCES `anotherTable`(`aColumn`, anotherColumn)',
-                null
+                'anotherTable',
+                [['aColumn', 0, null], ['anotherColumn', 0, null]],
+                null,
+                null,
+                null,
             ],
             'REFERENCES `anotherTable`(`aColumn`(199),`anotherColumn`)' => [
                 'REFERENCES `anotherTable`(`aColumn`(199),`anotherColumn`)',
-                null
+                'anotherTable',
+                [['aColumn', 199, null], ['anotherColumn', 0, null]],
+                null,
+                null,
+                null,
             ],
             'REFERENCES `anotherTable`(`aColumn`(199) ASC, anotherColumn DESC)' => [
                 'REFERENCES `anotherTable`(`aColumn`(199) ASC, anotherColumn DESC)',
-                null
+                'anotherTable',
+                [['aColumn', 199, 'ASC'], ['anotherColumn', 0, 'DESC']],
+                null,
+                null,
+                null,
             ],
             'REFERENCES anotherTable(aColumn) MATCH FULL' => [
                 'REFERENCES anotherTable(aColumn) MATCH FULL',
-                null
+                'anotherTable',
+                [['aColumn', 0, null]],
+                'FULL',
+                null,
+                null,
             ],
             'REFERENCES anotherTable(aColumn) MATCH PARTIAL' => [
                 'REFERENCES anotherTable(aColumn) MATCH PARTIAL',
-                null
+                'anotherTable',
+                [['aColumn', 0, null]],
+                'PARTIAL',
+                null,
+                null,
             ],
             'REFERENCES anotherTable(aColumn) MATCH SIMPLE' => [
                 'REFERENCES anotherTable(aColumn) MATCH SIMPLE',
-                null
+                'anotherTable',
+                [['aColumn', 0, null]],
+                'SIMPLE',
+                null,
+                null,
             ],
             'REFERENCES anotherTable(aColumn) ON DELETE RESTRICT' => [
                 'REFERENCES anotherTable(aColumn) ON DELETE RESTRICT',
-                null
+                'anotherTable',
+                [['aColumn', 0, null]],
+                null,
+                'RESTRICT',
+                null,
             ],
             'REFERENCES anotherTable(aColumn) ON DELETE CASCADE' => [
                 'REFERENCES anotherTable(aColumn) ON DELETE CASCADE',
-                null
+                'anotherTable',
+                [['aColumn', 0, null]],
+                null,
+                'CASCADE',
+                null,
             ],
             'REFERENCES anotherTable(aColumn) ON DELETE SET NULL' => [
                 'REFERENCES anotherTable(aColumn) ON DELETE SET NULL',
-                null
+                'anotherTable',
+                [['aColumn', 0, null]],
+                null,
+                'SET NULL',
+                null,
             ],
             'REFERENCES anotherTable(aColumn) ON DELETE NO ACTION' => [
                 'REFERENCES anotherTable(aColumn) ON DELETE NO ACTION',
-                null
+                'anotherTable',
+                [['aColumn', 0, null]],
+                null,
+                'NO ACTION',
+                null,
             ],
             'REFERENCES anotherTable(aColumn) ON UPDATE RESTRICT' => [
                 'REFERENCES anotherTable(aColumn) ON UPDATE RESTRICT',
-                null
+                'anotherTable',
+                [['aColumn', 0, null]],
+                null,
+                null,
+                'RESTRICT',
             ],
             'REFERENCES anotherTable(aColumn) ON UPDATE CASCADE' => [
                 'REFERENCES anotherTable(aColumn) ON UPDATE CASCADE',
-                null
+                'anotherTable',
+                [['aColumn', 0, null]],
+                null,
+                null,
+                'CASCADE',
             ],
             'REFERENCES anotherTable(aColumn) ON UPDATE SET NULL' => [
                 'REFERENCES anotherTable(aColumn) ON UPDATE SET NULL',
-                null
+                'anotherTable',
+                [['aColumn', 0, null]],
+                null,
+                null,
+                'SET NULL',
             ],
             'REFERENCES anotherTable(aColumn) ON UPDATE NO ACTION' => [
                 'REFERENCES anotherTable(aColumn) ON UPDATE NO ACTION',
-                null
+                'anotherTable',
+                [['aColumn', 0, null]],
+                null,
+                null,
+                'NO ACTION',
             ],
             'REFERENCES anotherTable(uid, `hash`(199) DESC) MATCH PARTIAL ON DELETE RESTRICT ON UPDATE SET NULL' => [
                 'REFERENCES anotherTable(uid, `hash`(199) DESC) MATCH PARTIAL ON DELETE RESTRICT ON UPDATE SET NULL',
-                null
+                'anotherTable',
+                [['uid', 0, null], ['hash', 199, 'DESC']],
+                'PARTIAL',
+                'RESTRICT',
+                'SET NULL',
             ],
         ];
     }
@@ -95,11 +171,47 @@ class ReferenceDefinitionTest extends TestCase
      * @test
      * @dataProvider canParseReferenceDefinitionDataProvider
      * @param string $columnAttribute
+     * @param string $table
+     * @param array $columns
+     * @param string $match
+     * @param string $onDelete
+     * @param string $onUpdate
      */
-    public function canParseReferenceDefinition(string $columnAttribute, $expectedResult)
-    {
+    public function canParseReferenceDefinition(
+        string $columnAttribute,
+        string $table,
+        array $columns,
+        string $match = null,
+        string $onDelete = null,
+        string $onUpdate = null
+    ) {
         $statement = sprintf('CREATE TABLE `aTable`(`aField` INT(11) %s)', $columnAttribute);
-        $subject = new Parser($statement);
-        $subject->parse();
+        $subject = $this->createSubject($statement);
+
+        $this->assertInstanceOf(ReferenceDefinition::class, $subject);
+        $this->assertSame($table, $subject->tableName->schemaObjectName);
+        $this->assertSame($match, $subject->match);
+        $this->assertSame($onDelete, $subject->onDelete);
+        $this->assertSame($onUpdate, $subject->onUpdate);
+
+        foreach ($columns as $index => $column) {
+            $this->assertSame($column[0], $subject->columnNames[$index]->columnName->schemaObjectName);
+            $this->assertSame($column[1], $subject->columnNames[$index]->length);
+            $this->assertSame($column[2], $subject->columnNames[$index]->direction);
+        }
+    }
+
+    /**
+     * Parse the CREATE TABLE statement and return the reference definition
+     *
+     * @param string $statement
+     * @return \MojoCode\SqlParser\AST\ReferenceDefinition
+     */
+    protected function createSubject(string $statement): ReferenceDefinition
+    {
+        $parser = new Parser($statement);
+        $createTableStatement = $parser->parse()[0];
+
+        return $createTableStatement->createDefinition->items[0]->reference;
     }
 }
